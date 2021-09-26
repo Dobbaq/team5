b6f428e252487af8bb4d577cf7572e745f486171
myckellr <myckellr@gmail.com>
2021-09-25 23:22:02 -0400

    Worked on get_valid_moves function

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -15,7 +15,38 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		Location temp = new Location(this.myLoc);
+		ArrayList<Location> validMoves = new ArrayList<Location>();
+		x = temp.x;
+		y = temp.y;
+		/*Can't go through wall. But Empty, Cookie, Ghost works*/
+		if(this.myMap.getLoc(this.temp(x - 1, y + 1)) != Map.Type.WALL) 
+		{
+			validMoves.add(this.temp(x - 1, y + 1)); //Top Left Diagonal
+		} else if(this.myMap.getLoc(this.temp(x - 1, y)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x - 1, y)); //Left
+		} else if(this.myMap.getLoc(this.temp(x - 1, y - 1)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x - 1, y - 1)); //Bottom Left Diagonal
+		} else if(this.myMap.getLoc(this.temp(x, y - 1)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x, y - 1)); //Bottom
+		} else if(this.myMap.getLoc(this.temp(x + 1, y - 1)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x + 1, y - 1)); //Bottom Right Diagonal
+		} else if(this.myMap.getLoc(this.temp(x + 1, y)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x + 1, y)); //Right
+		} else if(this.myMap.getLoc(this.temp(x + 1, y + 1)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x + 1, y + 1)); //Top Right Diagonal
+		} else if(this.myMap.getLoc(this.temp(x, y + 1)) != Map.Type.WALL)
+		{
+			validMoves.add(this.temp(x, y + 1)); //Top
+		}
+		
+		return validMoves;	
 	}
 
 	public boolean move() {
@@ -26,7 +57,7 @@ public class PacMan{
 		return false;
 	}
 
-	public JComponent consume() { 
+	public JComponent consume() {  
  		return null;
 	}
 }
