d4132b25a563536d10f8e52452fcb02e149691bf
Evan Nadelbach <evann710@gmail.com>
2021-09-26 20:26:24 -0400

    First implementation of is_pacman_in_range method in Ghost class

--- Projects/P2/src/Ghost.java
+++ Projects/P2/src/Ghost.java
@@ -21,6 +21,47 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
+		
+		// Bottom left
+		if (myMap.getLoc(new Location(myLoc.x - 1, myLoc.y - 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+		
+		// Bottom middle
+		else if (myMap.getLoc(new Location(myLoc.x, myLoc.y - 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Bottom right
+		else if (myMap.getLoc(new Location(myLoc.x + 1, myLoc.y - 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Middle left
+		else if (myMap.getLoc(new Location(myLoc.x - 1, myLoc.y)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Middle right
+		else if (myMap.getLoc(new Location(myLoc.x + 1, myLoc.y)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Top left
+		else if (myMap.getLoc(new Location(myLoc.x - 1, myLoc.y + 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Top middle
+		else if (myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
+		// Top right
+		else if (myMap.getLoc(new Location(myLoc.x + 1, myLoc.y + 1)).contains(Map.Type.PACMAN)) {
+			return true;
+		}
+
 		return false;
 	}
 
