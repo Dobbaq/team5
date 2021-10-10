7928fb075df42aa2038b8c717cdcbf5f00bf3802
rygore <rgorinson99@gmail.com>
2021-10-10 18:26:39 -0400

    stress

--- Projects/P2/tests/TestMapEatCookie.java
+++ Projects/P2/tests/TestMapEatCookie.java
@@ -5,6 +5,11 @@ import java.io.*;
 public class TestMapEatCookie {
 	
 	public void testMapEatCookie() {
-		return null;
+		Mainframe frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(4, 6)); //Creates PacMan at location 4,6 no cookie at 4,6
+
+		Map m = frame.getMap();
+
+		AssertEquals(m.eatCookie("pacman"), null);
 	}
 }
