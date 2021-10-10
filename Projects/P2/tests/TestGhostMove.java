ee0b613bb58ae4a3925d61bace55f57a043d69f4
Evan Nadelbach <evann710@gmail.com>
2021-10-10 18:01:45 -0400

    Wrote tests for ghost_move and fixed its method

--- Projects/P2/tests/TestGhostMove.java
+++ Projects/P2/tests/TestGhostMove.java
@@ -1,10 +1,24 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		MainFrame frame = new MainFrame();
+
+		Ghost ghost = frame.addGhost(new Location(1, 1), "test_ghost", Color.red);
+		Map map = frame.getMap();
+
+		ArrayList<Location> moves = ghost.get_valid_moves();
+		assertTrue(ghost.move());
+
+		boolean ghostInLoc = false;
+		if (map.getLoc(moves.get(0)).contains(Map.Type.GHOST) || map.getLoc(moves.get(1)).contains(Map.Type.GHOST)) {
+			ghostInLoc = true;
+		}
+
+		assertTrue(ghostInLoc);
 	}
 }
