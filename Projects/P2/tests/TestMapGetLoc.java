92fd1630b76fdb62e4c311e33984ca6392e1eef4
rygore <rgorinson99@gmail.com>
2021-10-10 18:09:10 -0400

    Test included

--- Projects/P2/tests/TestMapGetLoc.java
+++ Projects/P2/tests/TestMapGetLoc.java
@@ -1,11 +1,20 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-
+import java.util.ArrayList;
 
 public class TestMapGetLoc {
 	
 	public void testMapGetLoc() {
-		return null;
+		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		PacMan pacman = frame.addPacMan(new Location(2, 4)); //Creates PacMan at location x, y
+		Map m = frame.getMap();
+
+		ArrayList<Location> l = new ArrayList<>();
+	
+
+		AssertEquals(m.getLoc(new Location (2,4)), m.Type.PACMAN);
 	}
 }
