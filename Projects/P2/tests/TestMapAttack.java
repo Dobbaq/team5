fac0eda126c61ecb94622313852edc6ccadb1662
rygore <rgorinson99@gmail.com>
2021-10-10 18:20:38 -0400

    attack test

--- Projects/P2/tests/TestMapAttack.java
+++ Projects/P2/tests/TestMapAttack.java
@@ -4,6 +4,10 @@ import junit.framework.*;
 public class TestMapAttack {
 
 	public void testMapAttack() {
-		return null;	
+		Mainframe frame = new MainFrame();
+		Ghost ghost = frame.addGhost(new Location(9, 11), "Joe", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(9, 12)); //Creates PacMan at location x, y
+
+		AssertTrue(ghost.attack());
 	}
 }
