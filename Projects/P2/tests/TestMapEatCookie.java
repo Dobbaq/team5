e3626611596069e5b3879feb771df9a8ce5c3be0
rygore <rgorinson99@gmail.com>
2021-10-28 20:42:24 -0400

    ryan pushes

--- Projects/P2/tests/TestMapEatCookie.java
+++ Projects/P2/tests/TestMapEatCookie.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class TestMapEatCookie extends TestCase {
 	
 	public void testMapEatCookie() throws FileNotFoundException {
-		MainFrame frame = new MainFrame();
+		NoFrame frame = new NoFrame();
 		PacMan pacman = frame.addPacMan(new Location(4, 6)); //Creates PacMan at location 4,6 no cookie at 4,6
 
 		Map m = frame.getMap();
